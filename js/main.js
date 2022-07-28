0ccef9e15026b17789cc4ad6f555cc6588a12840
Dima <dmitryberesten@gmail.com>
2022-07-28 19:12:57 +0300

    Реализация клика для открыть/закрыть по заголовку виджета

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 
+
+// Фильтр на мобильных устройствах
 const sidebarToggleBtn = document.querySelector('.menu-icon-wrapper');
 const menuIcon = document.querySelector('.menu-icon');
 const sidebar = document.querySelector('.sidebar');
@@ -10,3 +12,25 @@ sidebarToggleBtn.onclick = function () {
     sidebar.classList.toggle('sidebar--mobile-active');
 };
 
+
+// Показать еще 3 карточки
+const btnShowMoreCards = document.querySelector('.btn-more')
+const hiddenCards = document.querySelectorAll('.card-link--hidden');
+
+btnShowMoreCards.addEventListener('click', function() {
+    hiddenCards.forEach(function (card) {
+        hiddenCards.classList.remove('card-link--hidden');
+    })
+})
+
+// Показать-скрыть контент внутри виджетов
+const widgets = document.querySelectorAll('.widget');
+
+widgets.forEach(function (widget) {
+    widget.addEventListener('click', function (e) {
+        if (e.target.classList.contains('widget__title')) {
+            e.target.classList.toggle('widget__title--active');
+            e.target.nextElementSibling.classList.toggle('widget__body--hiden');
+        }
+    })
+})
