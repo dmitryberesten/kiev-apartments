665a192a83305fa95014f5c3f58af31b9019d3bd
Dima <dmitryberesten@gmail.com>
2022-07-30 18:24:20 +0300

    Мелкие коррекции

--- js/main.js
+++ js/main.js
@@ -1,30 +1,39 @@
+/**
+ * NodeList.prototype.forEach() polyfill
+ * https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach#Polyfill
+ */
+ if (window.NodeList && !NodeList.prototype.forEach) {
+	NodeList.prototype.forEach = function (callback, thisArg) {
+		thisArg = thisArg || window;
+		for (var i = 0; i < this.length; i++) {
+			callback.call(thisArg, this[i], i, this);
+		}
+	};
+}
 
-
-// Фильтр на мобильных устройствах
+/* Фильтр на мобильный устроствах */
 const sidebarToggleBtn = document.querySelector('.menu-icon-wrapper');
 const menuIcon = document.querySelector('.menu-icon');
 const sidebar = document.querySelector('.sidebar');
 
-
-//  Клик по кнопке для скрытия / показа фильтра и изменения иконки
+// Клик по кнопке для скрытия / показа фильтра и изменения исконки
 sidebarToggleBtn.onclick = function () {
-    menuIcon.classList.toggle('menu-icon-active');
-    sidebar.classList.toggle('sidebar--mobile-active');
+	menuIcon.classList.toggle('menu-icon-active');
+	sidebar.classList.toggle('sidebar--mobile-active');
 };
 
-
-// Показать еще 3 карточки
-const btnShowMoreCards = document.querySelector('.btn-more')
+/* Показать еще 3 карточки */
+const btnShowMoreCards = document.querySelector('.btn-more');
 const hiddenCards = document.querySelectorAll('.card-link--hidden');
 
-// Клик по кнопке и показ скрытых карточек
-btnShowMoreCards.addEventListener('click', function() {
+// Клик по кнопке и показ трех скрытых карточек
+btnShowMoreCards.addEventListener('click', function () {
     hiddenCards.forEach(function (card) {
-        hiddenCards.classList.remove('card-link--hidden');
+        card.classList.remove('card-link--hidden');
     })
 })
 
-// Показать-скрыть контент внутри виджетов
+/* Показать/скрыть контент внутри виджетов */
 const widgets = document.querySelectorAll('.widget');
 
 // Находим все виджеты на странице
@@ -32,29 +41,28 @@ widgets.forEach(function (widget) {
 
     // Слушаем клик внутри виджета
     widget.addEventListener('click', function (e) {
-
-        // Если клик по заголовку - тогда скрываем/показываем тело виджета 
+        // Если клик по заголовку - тогда скрываем/показывае тело виджета
         if (e.target.classList.contains('widget__title')) {
             e.target.classList.toggle('widget__title--active');
-            e.target.nextElementSibling.classList.toggle('widget__body--hiden');
+            e.target.nextElementSibling.classList.toggle('widget__body--hidden');
         }
-    })
-})
+    });
+});
 
-// Location - кнопка Любая
+/* Location - кнопка Любая */
 const checkBoxAny = document.querySelector('#location-05');
 const topLocationCheckboxes = document.querySelectorAll('[data-location-param]');
 
-// Выбор кнопки Любая - и отключение других чекбоксов
+// Выбор кнопки Любая и отключение других чекбоксов
 checkBoxAny.addEventListener('change', function () {
     if (checkBoxAny.checked) {
-        topLocationCheckboxes.forEach(function (item){
-            item.checked = false;
-        });
+        topLocationCheckboxes.forEach(function (item) {
+			item.checked = false;
+		});
     }
 })
 
-// Клик по другим кнопкам в Location, отключаем кнопку "Любая", при выборе других параметров
+// Отключаем кнопку "Любая", при выборе других параметров
 topLocationCheckboxes.forEach(function (item) {
     item.addEventListener('change', function () {
         if (checkBoxAny.checked) {
@@ -63,30 +71,29 @@ topLocationCheckboxes.forEach(function (item) {
     })
 })
 
-// Показать еще 3 доп опции с чекбоксами в фильтре
+
+/* Показать еще 3 доп опции с чекбоксами в фильтре */
 const showMoreOptions = document.querySelector('.widget__btn-show-hidden');
 const hiddenCheckBoxes = document.querySelectorAll('.checkbox--hidden');
 
-showMoreOptions.onclick = function(e) {
-    e.preventDafault();
-
+showMoreOptions.onclick = function (e) {
+    e.preventDefault();
 
     // Если блоки были скрыты - значит показываем
-    if (showMoreOptions.dataset.options == "hidden") {
-        hiddenCheckBoxes.forEach(function (item) {
-            item.style.display = 'block';
-        })
-        showMoreOptions.innerText = "Скрыть дополнительные опции";
-        showMoreOptions.dataset.options = "visible";
+    if (showMoreOptions.dataset.options == 'hidden') {
+		hiddenCheckBoxes.forEach(function (item) {
+			item.style.display = 'block';
+		});
+		showMoreOptions.innerText = 'Скрыть дополнительные опции';
+		showMoreOptions.dataset.options = 'visible';
+	}
+	// Если блоки были видны - значит скрываем
+	else if (showMoreOptions.dataset.options == 'visible') {
+		hiddenCheckBoxes.forEach(function (item) {
+			item.style.display = 'none';
+		});
+		showMoreOptions.innerText = 'Показать ещё';
+		showMoreOptions.dataset.options = 'hidden';
+	}
 
-    } 
-    // Если блоки были видны - значит скрываем
-    else if (showMoreOptions.dataset.options == "visible") {
-        hiddenCheckBoxes.forEach(function (item) {
-            item.style.display = 'none';
-        })
-        showMoreOptions.innerText = "Показать ещё";
-        showMoreOptions.dataset.options = "hidden";
-    }
-    
 }
